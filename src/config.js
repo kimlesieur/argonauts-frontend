11279c55d2893af9d62ceec1a6e7e2b5209407f0
Kim Lesieur <lesieurkim@gmail.com>
2022-09-30 19:16:13 +0200

    fix: heroku config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const ENV = {
     API_URL: process.env.REACT_APP_API_URL,
     TEST_URL: process.env.REACT_APP_TEST_URL,
-    PRODUCTION : process.env.PRODUCTION,
+    PRODUCTION : process.env.REACT_APP_PRODUCTION,
 };
 
 
